af34dcf749b2cf8b4b61f2674a12ea61fcf75e72
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-08-17 00:13:26 +0530

    add project

---
+++ src/main/java/com/demo/pagearc/PageArcApplication.java
@@ -0,0 +1,13 @@
+package com.demo.pagearc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageArcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageArcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/pagearc/PageArcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pagearc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageArcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
