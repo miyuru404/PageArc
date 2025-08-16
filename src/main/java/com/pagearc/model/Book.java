e15dd03fcd4838fc6d0c7454afb7dc0cb7b8fb7f
Miyuru Bashitha <miyurubashitha100@gmail.com>
2025-08-17 00:24:50 +0530

    add project structure

---
+++ src/main/java/com/pagearc/model/Book.java
@@ -0,0 +1,4 @@
+package com.pagearc.model;
+
+public class Book {
+}
--- src/test/java/com/pagearc/PageArcApplicationTests.java
+++ src/test/java/com/pagearc/PageArcApplicationTests.java
@@ -1,4 +1,4 @@
-package com.demo.pagearc;
+package com.pagearc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
